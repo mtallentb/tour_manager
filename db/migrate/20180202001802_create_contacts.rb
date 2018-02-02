3439f77e9597e2388904d2ef0d331f111f315deb
Matt Brown <mtallentb.design@gmail.com>
2018-02-01 20:01:55 -0600

    base files

---
+++ db/migrate/20180202001802_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :company
+      t.string :position
+      t.string :info
+
+      t.timestamps
+    end
+  end
+end
